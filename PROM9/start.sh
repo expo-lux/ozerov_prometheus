edc592a9625033225aa045cbb173b300f8b1d048
Dmitry Shavrin <d.shavrin@arenadata.io>
2023-11-13 11:16:23 +0500

    PROM9

---
+++ PROM9/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+ansible-galaxy install --roles-path "$SCRIPT_DIR/roles" -r requirements.yml
+#ansible-galaxy collection install -p "$SCRIPT_DIR/collections" -r requirements_collections.yml
+#ANSIBLE_COLLECTIONS_PATH="$SCRIPT_DIR/collections" 
+ansible-playbook -i inventories/rbr deploy_prom.yml 
+
+echo "run 'forkstat -e all -S' in case of any problems"
